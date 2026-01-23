fc6557c68ed9633906c7f1340952a44e6e189ad9
PURIWAT PAHUSUWANNO <puriwat.p@ku.th>
2026-01-23 15:38:24 +0700

    five commit

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate         
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +16,7 @@ class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(app, model_class=Base)
+migrate = Migrate(app, db)   
 
 class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -26,8 +30,7 @@ class TodoItem(db.Model):
             "done": self.done
         }
 
-with app.app_context():
-    db.create_all()
+
     
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),

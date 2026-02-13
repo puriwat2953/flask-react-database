2052f3ed7ddeb0f3762edbc4169b6649ff3ede57
PURIWAT PAHUSUWANNO <puriwat.p@ku.th>
2026-02-13 15:41:53 +0700

    ex2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, 
+    environment: 'jsdom',                   // รันเทสแบบไม่มี browser
+    setupFiles: './src/setupTests.js',      // ระบุโค้ดสำหรับเตรียมต่าง ๆ
+  },
 })

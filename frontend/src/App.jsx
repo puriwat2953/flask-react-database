d1a14c0c25b2f2c08fc315b87c99faaf8638f1ef
PURIWAT PAHUSUWANNO <puriwat.p@ku.th>
2026-02-20 11:24:29 +0700

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import TodoItem from './TodoItem.jsx'
 
 import './App.css'
